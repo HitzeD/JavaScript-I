738f38654ecaa6794ee49899459735334bd00f81
HitzeD <d-hitze@comcast.net>
2019-04-01 13:59:54 -0500

    finished challenge one, assignment one

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,40 @@ const example = {
 }
 
 // Write your intern objects here:
+const int1 = {
+  {'id': 1,
+  'name': 'Mitzi',
+  'email': 'mmelloy0@psu.edu',
+  'gender': 'F',},
+};
 
+const int2 = {
+  {'id': 2,
+  'name': 'Kennan',
+  'email': 'kdiben1@tinypic.com',
+  'gender': 'M',},
+};
+
+const int3 = {
+  {'id': 3,
+  'name': 'Keven',
+  'email': 'mummery2@wikimedia.org',
+  'gender': 'M',},
+};
+
+const int4 = {
+  {'id': 4,
+  'name': 'Gannie',
+  'email': 'gmartinson3@illinois.edu',
+  'gender': 'M',},
+};
+
+const int5 = {
+  {'id': 5,
+  'name': 'Antonietta',
+  'email': 'adaine5@samsung.com',
+  'gender': 'F',},
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
